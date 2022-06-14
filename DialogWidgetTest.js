268ae6a101fbf7a4878f1ada71ae5e47cf1d1352
dogboydog <dogboydog@users.noreply.github.com>
2022-06-14 08:30:49 -0400

    Update DialogWidgetTest.js

--- DialogWidgetTest.js
+++ DialogWidgetTest.js
@@ -9,13 +9,6 @@ DialogWidgetTest.testPromptAction = tiled.registerAction("DialogWidgetTest", fun
 
     dialog.addLabel("My cool label\n");
     dialog.addSeparator();
-    // var sizeXInputArgs = new NumberInputArgs();
-    // sizeXInputArgs.suffix ="px";
-    // sizeXInputArgs.maximum = 4080;
-    // sizeXInputArgs.minimum = 1;
-    // sizeXInputArgs.label = "Size X:";
-    // var sizeXInput = dialog.addNumberInput(sizeXInputArgs);
-    
     var slider = dialog.addSlider("My slider");
     dialog.show();
 });
